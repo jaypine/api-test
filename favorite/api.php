be29859188f87432876306889abe52bc1a4ad78f
libingqi <libingqi@bilibili.com>
2017-10-19 22:01:53 +0800

    folder

--- favorite/api.php
+++ favorite/api.php
@@ -8,6 +8,7 @@ class API
         $this->appkey = $appkey;
         $this->appsecret = $appsecret;
         $this->ts = time();
+        $this->cookie = "";
     }
 
     public function httpGet($url,$params)
@@ -21,38 +22,51 @@ class API
         print($url);
         print("\n=======\n");
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($curl, CURLOPT_URL, $url);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 500);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 
-        $res = curl_exec($curl);
-        curl_close($curl);
+        if(!empty($this->cookie)){
+            curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
+        }
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
 
         return $res;
     }
 
     public function httpPost($url,$params)
     {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
-        curl_setopt($curl, CURLOPT_POST, 1);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 500);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
 
         $params["ts"] = $this->ts;
-        $params['appkey'] = $this->appkey;
+        $params["appkey"] = $this->appkey;
         $params["sign"] = $this->getSign($params);
 
         print($url.'?'.http_build_query($params));
         print("\n=======\n");
 
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-        $res = curl_exec($curl);
-        curl_close($curl);
+        if(!empty($this->cookie)){
+            curl_setopt($curl, CURLOPT_COOKIE, $this->cookie);
+        }
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $res = curl_exec($ch);
+
+        // var_dump(curl_error($ch));
+
+        curl_close($ch);
 
         return $res;
     }
--- favorite/folder.php
+++ favorite/folder.php
@@ -6,6 +6,6 @@ $apitest = new API("0dc647722719f2ea","a3769b553680da10b40994265fb48d20");
 $params=array(
     "mid"=>88888894
 );
-$res = $apitest->httpGet("http://api.bilibili.com/x/internal/v2/fav/folder",$params);
+$res = $apitest->httpGet("http://api.bilibili.co/x/internal/v2/fav/folder",$params);
 
 var_dump($res);
